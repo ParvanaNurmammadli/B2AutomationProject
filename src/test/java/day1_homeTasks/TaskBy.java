b98c7404ba39e31fe71d4283944cde6a73e66775
ParvanaNurmammadli <parvana.nurmammadli@gmail.com>
2023-12-07 22:23:43 -0500

    test5

--- src/test/java/day1_homeTasks/TaskBy.java
+++ src/test/java/day1_homeTasks/TaskBy.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TaskByRamiz {
+public class TaskBy {
 
     public static void main(String[] args) {
 
